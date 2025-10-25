fe50e6dcde959098b2349840852a869c7edf86f1
李叶煊 <liyexuan23@mails.tsinghua.edu.cn>
2025-10-24 19:21:31 -0700

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -47,14 +47,6 @@ function pop(imageURL, linkURL) {
     }
     toggleClass(".tc-main", "active");
     toggleClass(".tc", "active");
-
-    // 点击图片跳转到指定链接
-    tcMainElement.onclick = function(event) {
-        event.stopPropagation(); // 防止冒泡影响关闭弹窗
-        if (linkURL) {
-            window.location.href = linkURL;
-        }
-    };
 }
 
 var tc = document.getElementsByClassName('tc');
